c2ea5d0d1f1c763fd26c028ba02780249d0a2237
Daniel Martin <djm_djm@hotmail.com>
2020-06-28 16:14:12 +0100

    Add styling to Footer component

---
+++ src/components/Footer/stories/Footer.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Footer } from '../src';
+
+export default {
+	title: 'Footer',
+	component: Footer,
+};
+
+export const Story = () => {
+	return <Footer />;
+};
