cd38cb1e1e717edc60b9e69e720dcac99a11a0c2
Daniel Martin <djm_djm@hotmail.com>
2020-07-02 00:22:07 +0100

    Add Link around Banner logo image

--- src/components/Banner/src/Banner.tsx
+++ src/components/Banner/src/Banner.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { BannerProps } from './types';
 import { Navbar } from './subcomponents/Navbar/src';
 import './Banner.scss';
@@ -14,7 +15,9 @@ export const Banner: React.FC<BannerProps> = ({
 		<div className="banner">
 			<div className="app-content-container">
 				<div className="banner__logo">
-					<img src={spineLogo} alt="Highworth Physiotherapy Clinic logo" />
+					<Link to="/">
+						<img src={spineLogo} alt="Highworth Physiotherapy Clinic logo" />
+					</Link>
 				</div>
 				<div className="banner__title">{name}</div>
 				<div className="banner__address">{address}</div>
