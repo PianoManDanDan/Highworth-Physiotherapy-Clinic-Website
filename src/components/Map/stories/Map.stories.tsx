83502c94980cae4095e1860d9d652f562becd457
Daniel Martin <djm_djm@hotmail.com>
2020-08-27 16:35:46 +0100

    Pull Map iframe into own component

---
+++ src/components/Map/stories/Map.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Map } from '../src';
+
+export default {
+	title: 'Map',
+	component: Map,
+};
+
+export const Story = () => {
+	return <Map />;
+};
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PageHeading } from '../components/PageHeading/src';
 import { PageSubHeading } from '../components/PageSubHeading/src';
 import { OpeningHours } from '../components/OpeningHours/src';
+import { Map } from '../components/Map/src';
 import { openingHoursData } from './data';
 
 export const Contact: React.FC<{}> = () => {
@@ -18,18 +19,7 @@ export const Contact: React.FC<{}> = () => {
 							We are located at 13 High Street, Highworth, just down from the
 							main square, located between the King and Queen pub and the old
 							bakery.
-							<div className="location__map">
-								<iframe
-									title="Map to Highworth Physiotherapy Clinic"
-									// eslint-disable-next-line max-len
-									src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d619.1503644021743!2d-1.710772826512844!3d51.630509387569745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487148625caf79a5%3A0xfd8b60c3a023fce!2sHighworth%20Physiotherapy%20Clinic!5e0!3m2!1sen!2suk!4v1593645052277!5m2!1sen!2suk"
-									allowFullScreen
-									frameBorder={0}
-									aria-hidden={false}
-									// eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-									tabIndex={0}
-								/>
-							</div>
+							<Map />
 						</div>
 						<div className="opening-hours-section">
 							<PageSubHeading text="Opening Hours" />
