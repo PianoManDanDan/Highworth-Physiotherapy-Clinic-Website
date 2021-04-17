e8399657f0cf2af0e83b212f694e91328d5eaeaf
Daniel Martin <djm_djm@hotmail.com>
2021-04-18 00:26:13 +0100

    Add plop generator for Component files

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - [x] Set up `next.config.js`
 - [x] Set up typescript
 - [x] Add plopfile and .hbs template files for pages
-- [ ] Add plopfile and .hbs template files for components
+- [x] Add plopfile and .hbs template files for components
 - [x] Set up testing (install deps and add jest config - see [here](https://github.com/vercel/next.js/tree/canary/examples/with-typescript-eslint-jest))
 - [x] Set up storybook
 - [x] Set up docker
--- plopfile.js
+++ plopfile.js
@@ -26,4 +26,46 @@ module.exports = (plop) => {
       }
     ],
   });
+  plop.setGenerator('Component', {
+    description: 'Generate a new component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Pick a name for the Component:',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'plop-templates/Component/Component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'components/{{pascalCase name}}/{{pascalCase name}}Props.ts',
+        templateFile: 'plop-templates/Component/ComponentProps.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: 'components/{{pascalCase name}}/{{pascalCase name}}.module.scss',
+        templateFile: 'plop-templates/Component/Component.module.scss.hbs',
+      },
+      {
+        type: 'add',
+        path: 'components/{{pascalCase name}}/index.ts',
+        templateFile: 'plop-templates/Component/index.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: 'stories/{{pascalCase name}}.stories.tsx',
+        templateFile: 'plop-templates/Component/Component.stories.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'tests/components/{{pascalCase name}}.test.tsx',
+        templateFile: 'plop-templates/Component/Component.test.tsx.hbs',
+      },
+    ],
+  });
 }
