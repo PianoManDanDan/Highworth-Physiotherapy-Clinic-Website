7b7451be67e8029c90a00295255cabcf936fee50
Daniel Martin <djm_djm@hotmail.com>
2021-04-27 01:06:19 +0100

    Add _app, global styles and 404 fallback page

---
+++ pages/404.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+
+export const PageNotFound = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>Oops... | Highworth Physiotherapy Clinic</title>
+      </Head>
+      <main>
+        <Image
+          src='/assets/men-at-work.jpg'
+          alt='Construction workers building a spine'
+          height={448}
+          width={336}
+          layout='intrinsic'
+        />
+        <h1>Oops...</h1>
+        <p>
+          It looks like we can't find the page you are looking for. Please try 
+          navigating to a different page.
+          </p>
+      </main>
+    </>
+  );
+}
+
+export default PageNotFound;
---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+import '../styles/_global.scss';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
