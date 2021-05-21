1651391ed5abceebc92233d746fa7a3c3f03965b
Daniel Martin <djm_djm@hotmail.com>
2021-05-22 00:08:27 +0100

    Add layout styles to Header component
    - Add classes to Header component, along with extra layout tags needed
      to style the Header correctly
    - Add layout styles to Header.module.scss

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
-import cn from 'classnames';
 import Image from 'next/image';
+import Link from 'next/link';
+import { Phone } from 'react-feather';
 import { NavMenu, NavMenuItem } from '../NavMenu';
 import styles from './Header.module.scss';
 
 export const Header: React.FC<{}> = () => {
   return (
     <header className={styles.header}>
-      <Image
-        src='/assets/logos/hpc.png'
-        alt='Highworth Physiotherapy Clinic'
-        height={128}
-        width={30}
-        layout='intrinsic'
-      />
-      <h1>Highworth Physiotherapy Clinic</h1>
-      <address>
-        <h2>tel: <a href="tel:+441793763814">01793 763814</a></h2>
-      </address>
+      <div className={styles['content-wrapper']}>
+        <Link href='/'>
+          <a className={styles.logo}>
+            <Image
+              src='/assets/logos/hpc.png'
+              alt='Highworth Physiotherapy Clinic'
+              height={128}
+              width={30}
+              layout='intrinsic'
+            />
+          </a>
+        </Link>
+        <div className={styles['text-wrapper']}>
+          <Link href='/'>
+            <a>
+              <h1 className={styles.title}>Highworth Physiotherapy Clinic</h1>
+            </a>
+          </Link>
+          <address>
+            <h2>
+              <a
+                className={styles.contact}
+                href="tel:+441793763814"
+              >
+                <Phone />
+                <span className={styles.contact__number}>01793 763814</span>
+              </a>
+            </h2>
+          </address>
+        </div>
+      </div>
       <NavMenu>
         <NavMenuItem text='Home' href='/' />
         <NavMenuItem text='Facilities' href='/facilities' />
@@ -29,6 +50,6 @@ export const Header: React.FC<{}> = () => {
       </NavMenu>
     </header>
   );
-}
+};
 
 export default Header;
