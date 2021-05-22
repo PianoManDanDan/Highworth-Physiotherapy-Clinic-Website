b29e0286e690ebb5ed81f6a39690584efa03947e
Daniel Martin <djm_djm@hotmail.com>
2021-05-22 15:50:37 +0100

    Add layout styling to Footer component
    - Add classes to tags in Footer component
    - Add layout styling to Footer.module.scss

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,89 +1,87 @@
 import React from 'react';
-import Image from 'next/image';
+import { Facebook } from 'react-feather';
 import styles from './Footer.module.scss';
 
 export const Footer: React.FC<{}> = () => {
   return (
-    <footer>
-      <div>
-        <p>Find us on Facebook</p>
-        <a 
-          href="https://www.facebook.com/HighworthPhysio/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src='/assets/logos/facebook.png'
-            alt='Find us on Facebook'
-            height={32}
-            width={32}
-            layout='intrinsic'
-          />
-        </a>
-      </div>
-      <div>
-        <p>
-          All of our physios are registered with at least one of the following regulatory bodies:
-        </p>
-        <ul>
-          <li>
+    <footer className={styles.footer}>
+      <div className={styles['footer__content-wrapper']}>
+        <div className={styles['social-media']}>
+          <p>Find us on Social Media</p>
+          <div className={styles['social-media__links']}>
             <a
-              href="https://www.csp.org.uk/"
+              href="https://www.facebook.com/HighworthPhysio/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <abbr title="Chartered Society of Physiotherapists">CSP</abbr>
+              <Facebook size={32} />
             </a>
-          </li>
-          <li>
-            <a
-              href="https://www.hcpc-uk.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <abbr title="Health and Care Professions Council">HCPC</abbr>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.physiofirst.org.uk/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Physio First
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.aacp.org.uk/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <abbr title="Acupuncture Association of Chartered Physiotherapists">AACP</abbr>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.acpat.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <abbr title="Association of Chartered Physiotherapists in Animal Therapy">ACPAT</abbr>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.rampregister.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <abbr title="Register of Animal Musculoskeletal Practitioners">RAMP</abbr>
-            </a>
-          </li>
-        </ul>
+          </div>
+        </div>
+        <div className={styles['external-links']}>
+          <p>
+            All of our physios are registered with at least one of the following regulatory bodies:
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://www.csp.org.uk/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <abbr title="Chartered Society of Physiotherapists">CSP</abbr>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.hcpc-uk.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <abbr title="Health and Care Professions Council">HCPC</abbr>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.physiofirst.org.uk/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Physio First
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.aacp.org.uk/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <abbr title="Acupuncture Association of Chartered Physiotherapists">AACP</abbr>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.acpat.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <abbr title="Association of Chartered Physiotherapists in Animal Therapy">ACPAT</abbr>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.rampregister.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <abbr title="Register of Animal Musculoskeletal Practitioners">RAMP</abbr>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
